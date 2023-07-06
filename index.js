842687b984c097a4db6c69713bd87167d59c0804
Julio Sanjuan <julio.sanjuan@dacodes.com.mx>
2023-07-05 20:55:49 -0600

    added timer feature

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const moment = require('moment')
+
 console.log('Hello World')
+
+const tick = () => {
+    console.clear()
+    console.log('Tick', moment().format('h:mm:ss a'))
+}
+
+setInterval(tick, 1000)
+
